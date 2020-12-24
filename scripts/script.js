622fb1b16d3505c7d7e295c69fa97fd90ea39a4e
dumb-animal <dumb_animal@icloud.com>
2020-12-24 16:27:03 +0300

    initial

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+  //==================TABS=======================
+  const tabs = [
+    document.getElementById('tab_01'),
+    document.getElementById('tab_02'),
+    document.getElementById('tab_03')
+  ]
+  const links = [
+    document.getElementById('tab-link_01'),
+    document.getElementById('tab-link_02'),
+    document.getElementById('tab-link_03')
+  ]
+
+  const showTab = ({ target }) => {
+    const id = parseInt(target.id[target.id.length - 1]) - 1
+
+    if (links[id].classList.contains('disabled')) return null
+
+    links.forEach((link) => link.classList.remove('active'))
+    tabs.forEach((tab) => (tab.style.display = 'none'))
+
+    links[id].classList.add('active')
+    tabs[id].style.display = 'block'
+  }
+
+  links.forEach((link) => link.addEventListener('click', showTab))
+
+  //==================SHOW ADS=======================
+
+  const leftAds = document.querySelector('cf-left-game-cont')
+  const rightAds = document.querySelector('cf-right-game-cont')
+  const game = document.querySelector('cf-game-cont')
+
+  const chackSize = () => {
+    leftAds.style.display = 'inline-block'
+    rightAds.style.display = 'inline-block'
+
+    let size = leftAds.offsetWidth + rightAds.offsetWidth + game.offsetWidth
+    if (size > window.innerWidth) leftAds.style.display = 'none'
+    size = rightAds.offsetWidth + game.offsetWidth
+    if (size > window.innerWidth) rightAds.style.display = 'none'
+  }
+
+  chackSize()
+  window.addEventListener("resize",chackSize)
+})
